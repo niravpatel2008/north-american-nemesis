f6335f869a278dce9c9599272598663d55dae707
unknown <mitesh24shah@gmail.com>
2015-05-27 16:37:15 +0530

    mitesh changes

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -16,6 +16,7 @@ class Signup extends CI_Controller {
 
             $post = $this->input->post();
             if ($post) {
+
                 $this->form_validation->set_rules('fname', 'First Name', 'trim|required');
                 $this->form_validation->set_rules('lname', 'Last Name', 'trim|required');
 				$this->form_validation->set_rules('email', 'Email', 'valid_email|trim|required|is_unique[users.u_email]');
@@ -29,12 +30,15 @@ class Signup extends CI_Controller {
 					switch($packageId)
 					{
 						case 1:
-							$expDate=Date('Y-m-d', strtotime("+30 days"));
+							$expDate=Date('Y-m-d', strtotime("+20 days"));
 						break;
 						case 2:
-							$expDate=Date('Y-m-d', strtotime("+180 days"));
+							$expDate=Date('Y-m-d', strtotime("+30 days"));
 						break;
 						case 3:
+							$expDate=Date('Y-m-d', strtotime("+180 days"));
+						break;
+						case 4:
 							$expDate=Date('Y-m-d', strtotime("+365 days"));
 						break;
 					}
@@ -57,32 +61,39 @@ class Signup extends CI_Controller {
 						'up_status' => 'Active'
                     );
 					
-					/* Paypal payment code */
-					$this->load->helper('paypal');
-					$paypal = new wp_paypal_gateway (true);
-				 
-					// Required Parameter for the getExpresscheckout
-					$param = array(
-						'amount' => 200,
-						'currency_code' => 'USD',
-						'payment_action' => 'Sale',
-					);
-					$param["return_url"] = base_url().PAYPAL_API_RETURN;
-					$param["cancel_url"] = base_url().PAYPAL_API_CANCEL;
-					
-					// Display the response if successful or the debug info
-					if ($paypal->setExpressCheckout($param)) {
-						$res=$paypal->getResponse();
-						$url = $paypal->getRedirectURL();
-						$payment["payment"] =  $paypal->getResponse();
-						$payment["user_data"] =  $insert_data;
-						$payment["plan_data"] =  $plan_data;
-						$this->session->set_userdata('payment_session', $payment);
-						echo $url;
-					} else {
-						print_r($paypal->debug_info);
+					if($packageId != 1)
+					{
+						/* Paypal payment code */
+						$this->load->helper('paypal');
+						$paypal = new wp_paypal_gateway (true);
+					 
+						// Required Parameter for the getExpresscheckout
+						$param = array(
+							'amount' => 200,
+							'currency_code' => 'USD',
+							'payment_action' => 'Sale',
+						);
+						$param["return_url"] = base_url().PAYPAL_API_RETURN;
+						$param["cancel_url"] = base_url().PAYPAL_API_CANCEL;
+						
+						// Display the response if successful or the debug info
+						if ($paypal->setExpressCheckout($param)) {
+							$res=$paypal->getResponse();
+							$url = $paypal->getRedirectURL();
+							$payment["payment"] =  $paypal->getResponse();
+							$payment["user_data"] =  $insert_data;
+							$payment["plan_data"] =  $plan_data;
+							$this->session->set_userdata('payment_session', $payment);
+							echo $url;
+						} else {
+							print_r($paypal->debug_info);
+						}
+						exit;
+					}else
+					{
+						
+						$this->processUsersInformation($insert_data,$plan_data);
 					}
-					exit;
                 }
 				else
 				{
@@ -101,6 +112,47 @@ class Signup extends CI_Controller {
         $this->load->view('content', $data);
     }
 	
+	public function processUsersInformation($insert_data,$plan_data)
+	{
+		$ret = $this->common_model->insertData('users', $insert_data);
+		$plan_data['up_u_id']=$ret;
+		$plan = $this->common_model->insertData('user_plan', $plan_data);
+		$u_email = $insert_data['u_email'];
+		# create session
+		$data = array('u_id' => $ret,
+			'u_email' => $u_email,
+			'u_name'=> $insert_data['u_fname'].' '.$insert_data['u_lname']
+		);
+		$this->session->set_userdata('front_session', $data);
+
+		if ($plan > 0) {
+			$this->common_model->setupApplication($plan_data);
+			
+			## send mail
+			//$login_details = array('u_email' => $user[0]->email,'u_password' => $newpassword);
+			$userRes = $user[0];
+			$emailTpl = $this->load->view('email_templates/signup', '', true);
+
+			$search = array('{name}','{username}','{OrgName}');
+			$replace = array($insert_data['u_fname']." ".$insert_data['u_lname'],$insert_data['u_email'],'ChatAdmin');
+			$emailTpl = str_replace($search, $replace, $emailTpl);
+
+			$ret = sendEmail($u_email, SUBJECT_LOGIN_INFO, $emailTpl, FROM_EMAIL, FROM_NAME);
+
+			$flash_arr = array('flash_type' => 'success',
+				'flash_msg' => 'Welcome to DX chat.'
+			);
+			$retFlg = 1;
+		} else {
+			$flash_arr = array('flash_type' => 'error',
+				'flash_msg' => 'An error occurred while processing.'
+			);
+			$retFlg = 0;
+		}
+		$this->session->set_flashdata('flash_arr', $flash_arr);
+		//echo base_url()."dashboard";exit;
+		redirect(base_url()."dashboard");
+	}
 
 	public function returnpay() {
 					$get = $this->input->get();
@@ -117,43 +169,8 @@ class Signup extends CI_Controller {
 
 					if ($payment['TOKEN'] != $token)
 						redirect(base_url());
-					
-					$ret = $this->common_model->insertData('users', $insert_data);
-					$plan_data['up_u_id']=$ret;
-					$plan = $this->common_model->insertData('user_plan', $plan_data);
-                    # create session
-                    $data = array('u_id' => $ret,
-                        'u_email' => $post['u_email']
-                    );
-                    $this->session->set_userdata('front_session', $data);
 
-                    if ($plan > 0) {
-						$this->common_model->setupApplication($plan_data,$insert_data);
-						
-						## send mail
-						//$login_details = array('u_email' => $user[0]->email,'u_password' => $newpassword);
-						$userRes = $user[0];
-						$emailTpl = $this->load->view('email_templates/signup', '', true);
-
-						$search = array('{name}','{username}','{password}','{OrgName}');
-						$replace = array($post['fname']." ".$post['lname'],$post['email'],$post['password'],'ChatAdmin');
-						$emailTpl = str_replace($search, $replace, $emailTpl);
-
-						$ret = sendEmail($userRes->u_email, SUBJECT_LOGIN_INFO, $emailTpl, FROM_EMAIL, FROM_NAME);
-
-						$flash_arr = array('flash_type' => 'success',
-                            'flash_msg' => 'Welcome to DX chat.'
-                        );
-						$retFlg = 1;
-                    } else {
-                        $flash_arr = array('flash_type' => 'error',
-                            'flash_msg' => 'An error occurred while processing.'
-                        );
-						$retFlg = 0;
-                    }
-                    $this->session->set_flashdata('flash_arr', $flash_arr);
-					
-					redirect(base_url()."dashboard");
+					$this->processUsersInformation($insert_data,$plan_data);
 	}
 
 	public function cancelpay() {

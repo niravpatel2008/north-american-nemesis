da481d0321896aac858befb415b32541b8f955a6
nirav patel <nirav.ce.2008@gmail.com>
2015-07-10 09:57:37 +0530

    New Chattool integrated

---
+++ chattool/settings/settings.ini.php
@@ -0,0 +1,9 @@
+<?php
+// $userdomain=array_shift(explode(".",$_SERVER['HTTP_HOST']));
+// if($userdomain!='')
+// {
+	// $settingFile=$userdomain.".settings.ini.php";
+	$config1=require("settings/ezcell123.settings.ini.php");
+	return $config1;
+//}
+?>

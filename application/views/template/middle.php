c701c807af2a51a914c05bdf0d05cc1f749fe258
unknown <patel.sanjay115@gmail.com>
2015-07-11 12:40:31 +0530

    Frontend design changes

--- application/views/template/middle.php
+++ application/views/template/middle.php
@@ -1,4 +1,4 @@
-<div class="container" style="padding:80px 0 0 0px;">
+<div class="container" style="padding:45px 0 0 0px;">
 <div id="flash_msg"></div>
 	<?php
 		$this->load->view($this->router->fetch_class()."/".$view);

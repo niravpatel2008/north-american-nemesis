bffc1d51e0f85ef6570ae253133eb8979281d9a4
Sanajy <patel.sanjay115@gmail.com>
2015-07-12 14:18:52 +0530

    chattool extra module remove

--- chattool/design/defaulttheme/tpl/lhsystem/configuration_tabs_content/chat_embed_js.tpl.php
+++ chattool/design/defaulttheme/tpl/lhsystem/configuration_tabs_content/chat_embed_js.tpl.php
@@ -4,13 +4,13 @@
 	<div class="row">
 	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/chat_embed.tpl.php'));?>
 	    	    
-	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/faq_embed.tpl.php'));?>
+	    <?php //include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/faq_embed.tpl.php'));?>
 	
-	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/questionary_embed.tpl.php'));?>
+	    <?php //include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/questionary_embed.tpl.php'));?>
 	
-	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/chatbox_embed.tpl.php'));?>
+	    <?php //include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/chatbox_embed.tpl.php'));?>
 	    
-	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/browse_offers_embed.tpl.php'));?>
+	    <?php //include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/browse_offers_embed.tpl.php'));?>
 	    
 	    <?php include(erLhcoreClassDesign::designtpl('lhsystem/configuration_links/embed_multiinclude.tpl.php'));?>
      </div>
